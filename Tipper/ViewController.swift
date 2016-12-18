2a8775514625928f9079a76a86d3254f16bb5d43
Madel Asistio <madelasistio@Madels-MacBook-Pro-3.local>
2016-12-18 08:20:17 -0800

    first commit

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipController: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func OnTap(_ sender: AnyObject) {
+        
+        // gets rid of the keyboard on tap
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.14, 0.16, 0.18, 0.20]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipController.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format:"$%.02f", tip)
+        totalLabel.text = String(format:"$%.02f",total)
+        
+    }
 
-}
+   }
 
