4a3c4da8826ead51ef9fde596baf3deb9e9bd0a3
Madel Asistio <madelasistio@Madels-MacBook-Pro-3.local>
2016-12-29 21:42:36 -0800

    cleaned up the view controller files and added descriptions

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -2,29 +2,32 @@
 //  SettingsViewController.swift
 //  Tipper
 //
-//  Created by Madel Asistio on 12/18/16.
-//  Copyright © 2016 Madel Asistio. All rights reserved.
+//  Created by Jie-Anne Asistio on 12/18/16.
+//  Copyright © 2016 Jie-Anne Asistio. All rights reserved.
 //
 
 import UIKit
 
 class SettingsViewController: UIViewController {
 
+    // button fields that hold the value for each segment control button in the main view controller
     @IBOutlet weak var bttn1Field: UITextField!
     @IBOutlet weak var bttn2Field: UITextField!
     @IBOutlet weak var bttn3Field: UITextField!
     @IBOutlet weak var bttn4Field: UITextField!
     
+    // array that stores the value from main view controller to record changes
     var percentages = [0.0, 0.0, 0.0, 0.0]
+    
+    // converts to the right format to display
     let multBy = 100.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
-        // Do any additional setup after loading the view.
     }
     
+    // changes the value of the numbers inside the text field depending on the current values of tip percentages
     override func viewWillAppear(_ animated: Bool) {
         bttn1Field.text = String (format: "%.0f", percentages[0] * multBy)
         bttn2Field.text = String (format: "%.0f", percentages[1] * multBy)
@@ -36,38 +39,4 @@ class SettingsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be rcreated.
     }
-    
-    /*
-    @IBAction func updatePercentage(_ sender: Any) {
-        let main = self.storyboard?.instantiateViewController(withIdentifier: "mainVC") as! ViewController
-        main.tipPercentages[0] = (Double(bttn1Field.text!) ?? 0) * shiftDec
-        main.tipPercentages[1] = (Double(bttn2Field.text!) ?? 0) * shiftDec
-        main.tipPercentages[2] = (Double(bttn3Field.text!) ?? 0) * shiftDec
-        main.tipPercentages[3] = (Double(bttn4Field.text!) ?? 0) * shiftDec
-        
-        
-        self.present(main, animated: true, completion: nil)
-    } */
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        
-        let main: ViewController = segue.destination as! ViewController
-        
-        main.tipPercentages[0] = (Double(bttn1Field.text!) ?? 0) * shiftDec
-        main.tipPercentages[1] = (Double(bttn2Field.text!) ?? 0) * shiftDec
-        main.tipPercentages[2] = (Double(bttn3Field.text!) ?? 0) * shiftDec
-        main.tipPercentages[3] = (Double(bttn4Field.text!) ?? 0) * shiftDec
-        
-        
-        
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    } */
-
 }
--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -2,27 +2,39 @@
 //  ViewController.swift
 //  Tipper
 //
-//  Created by Madel Asistio on 12/12/16.
-//  Copyright © 2016 Madel Asistio. All rights reserved.
+//  Created by Jie-Anne Asistio on 12/12/16.
+//  Copyright © 2016 Jie-Anne Asistio. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // label that shows the tip
     @IBOutlet weak var tipLabel: UILabel!
     
+    // label that shows the total
     @IBOutlet weak var totalLabel: UILabel!
     
+    // field where user types in bill amount
     @IBOutlet weak var billField: UITextField!
     
+    // tip segments
     @IBOutlet weak var tipController: UISegmentedControl!
     
+    // array of tip percentages
     var tipPercentages = [0.14, 0.16, 0.18, 0.20]
+    
+    // shifts the decimal value to change to
+    // the proper type for display
     let shiftDec = 0.01
     
+    // changes the button labels to proper values
+    // on the segment control every time it appears
     override func viewWillAppear(_ animated: Bool) {
         var counter = 0
         
+        // loops through the tip array and changes the segment control labels to settings
         for percent in tipPercentages {
             tipController.setTitle(String(format: "%.0f%%", percent * 100), forSegmentAt: counter)
             
@@ -30,17 +42,13 @@ class ViewController: UIViewController {
             
         }
     }
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        
-        // maybe create a loop that checks the array!! if it's 0.0 or not
-        // look at how to do persistence
         var counter = 0
         
+        // loops through the tip percentages array to load the correct numbers
         for percent in tipPercentages {
             tipController.setTitle(String(format: "%.0f%%", percent * 100), forSegmentAt: counter)
             
@@ -60,17 +68,20 @@ class ViewController: UIViewController {
         view.endEditing(true)
     }
     
+    // function calculates tip
     @IBAction func calculateTip(_ sender: AnyObject) {
     
-        // need to fix percentage amount but otherwise is good
+        // calculates the tip and total using the tip percentages array and the input bill
         let bill = Double(billField.text!) ?? 0
         let tip = bill * tipPercentages[tipController.selectedSegmentIndex]
         let total = bill + tip
         
+        // puts the correct value on the tip and total labels
         tipLabel.text = String(format:"$%.02f", tip)
         totalLabel.text = String(format:"$%.02f",total)
     }
     
+    // inputs the settings view controller with the values from the tip percentages
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "settingsSegue" {
             if let settingsVC = segue.destination as? SettingsViewController {
@@ -79,18 +90,18 @@ class ViewController: UIViewController {
         }
     }
     
-
-    
+    // changes the values depending on user input in settings
     @IBAction func updateFromSettings (segue:UIStoryboardSegue) {
         
+        // instantiates the settings view controller
         let settingsVC = segue.source as! SettingsViewController
+        
+        // changes the tip percentages depending on the value of the button fields
         tipPercentages[0] = (Double(settingsVC.bttn1Field.text!) ?? 0) * shiftDec
         tipPercentages[1] = (Double(settingsVC.bttn2Field.text!) ?? 0) * shiftDec
         tipPercentages[2] = (Double(settingsVC.bttn3Field.text!) ?? 0) * shiftDec
         tipPercentages[3] = (Double(settingsVC.bttn4Field.text!) ?? 0) * shiftDec
 
     }
-
-
    }
 
